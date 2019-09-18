e27d115f95c898af0be82de16cd16677a7a40108
kuro26 <hitcw26@163.com>
2019-09-18 21:47:05 +0800

    add julia env config script

---
+++ julia_env.sh
@@ -0,0 +1,22 @@
+# julia language
+cd ~
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.2/julia-1.2.0-linux-x86_64.tar.gz
+tar -zxvf julia-1.2.0-linux-x86_64.tar.gz
+rm julia-1.2.0-linux-x86_64.tar.gz
+# should be better way exist, we can check if the path exist
+echo "PATH=$PATH:$HOME/julia-1.2.0/bin" > .bashrc
+
+# editor vim
+mkdir .vim
+cd .vim &&
+git clone https://github.com/tpope/vim-pathogen.git
+cd ~
+echo "execute pathogen#infect()" >> .vimrc
+echo "syntax on" >> .vimrc
+echo "filetype plugin indent on" >> .vimrc
+cd .vim
+mkdir bundle && cd bundle &&
+git clone git://github.com/JuliaEditorSupport/julia-vim.git
+
+
+
